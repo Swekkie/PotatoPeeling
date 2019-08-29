b709a3d645fb911a3551a2bcf9df97e8dbebfd65
LAPTOP-INTIPOIC\jazzy <jazzysven@gmail.com>
2019-08-29 23:03:25 +0200

    clean up code

--- gui.py
+++ gui.py
@@ -8,6 +8,7 @@ fig, ax = plt.subplots()
 path1 = "D:/Masterproef/PotatoPeeling/pointset.txt"
 path2 = "D:/Masterproef/PotatoPeeling/foundpolygons.txt"
 
+
 with open(path1,"r") as f:
     points = f.read().splitlines()
 with open(path2,"r") as f:
@@ -20,21 +21,26 @@ for point in points:
 	xyvalues = point.split(";")
 	x.append(float(xyvalues[0]))
 	y.append(float(xyvalues[1][:-1]))
-	print(xyvalues[0] +"		"+ xyvalues[1][:-1])
+	#print(xyvalues[0] +"		"+ xyvalues[1][:-1])
 
-
-ax.scatter(x,y)
+ax.scatter(x,y,5)
 axes = fig.gca();
-axes.set_xlim([0,40])
-axes.set_ylim([0,40])
+
+axes.set_xlim([0,10])
+axes.set_ylim([0,10])
+axes.set_aspect('equal', adjustable='box')
+
 
 #add labels
+#'''
 for i in range(0,len(x)) :
     ax.annotate(
         i+1,
         xy=(x[i], y[i]), xytext=(0, 5),
         textcoords='offset points')
+#'''
 
+# construct polygons
 polygonList = []
 for polygon in foundpolygons:
     xCoordinates = []
@@ -56,16 +62,18 @@ for polygon in foundpolygons:
 numberOfPolygons = len(polygonList)
 print(len(polygonList))
 
+# switch polygon to plot
 def switchBetweenPolygons(num, polygonList):
     previous = num - 1
     if previous < 0:
         previous = len(polygonList)-1
+    print(num)
     polygonList[num].set_visible(True)
     polygonList[previous].set_visible(False)
 
     return polygonList
 
-ani = animation.FuncAnimation(fig, switchBetweenPolygons, numberOfPolygons, fargs=(polygonList,), interval=500, blit = False)
+ani = animation.FuncAnimation(fig, switchBetweenPolygons, numberOfPolygons, fargs=(polygonList,), interval=2000, blit = False)
 #comment above line and uncomment line below to show biggest polygon only
 #polygonList[0].set_visible(True)
 
